3253030f53cda3f8488f0ab71bbe19a3a18fb875
Nicolas Olenev <cardinal2113@gmail.com>
2022-02-17 04:07:07 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,46 @@
 import { UI } from './view.js'
 
+const authorization = {
+  title: 'Авторизация',
+  form_name: 'Почта:',
+  placeholder: '',
+  btn_name: 'Получить код',
+}
+
+const settings = {
+  title: 'Настройки',
+  form_name: 'Имя в чате',
+  placeholder: 'имя',
+  btn_name: '->',
+}
+
+const verification = {
+  title: 'Подтверждение',
+  form_name: 'Код:',
+  placeholder: '',
+  btn_name: 'Войти',
+}
+
 scrollChatWindowToBottom();
 
 UI.MESSAGE_FORM.addEventListener('submit', function () {
-  if (!UI.TEXT_FIELD.value) return
+  if (!UI.MESSAGE_INPUT.value) return
   const message = UI.MESSAGE_TEMPLATE.content.cloneNode('deep');
-  message.querySelector('.message').innerText = 'Я: ' + UI.TEXT_FIELD.value;
+  message.querySelector('.message').innerText = 'Я: ' + UI.MESSAGE_INPUT.value;
   this.reset();
   message.querySelector('.time').innerText = getTime();
   UI.CHAT_WINDOW.append(message);
   scrollChatWindowToBottom();
 });
 
+
 UI.SETTINGS_BTN.addEventListener('click', function () {
-  UI.SETTINGS_MENU.classList.add('visible');
+  getPopup(settings);
 });
 
-UI.SETTINGS_EXIT.addEventListener('click', function () {
-  UI.SETTINGS_MENU.classList.remove('visible');
+UI.EXIT_BTN.addEventListener('click', function () {
+  getPopup(authorization);
 });
-
-
-
 
 
 // <-- F U N C T I O N S -->
@@ -37,4 +56,63 @@ function getTime() {
 
 function scrollChatWindowToBottom() {
   UI.CHAT_WINDOW.scrollTop = UI.CHAT_WINDOW.scrollHeight;
+}
+
+function getPopup(obj) {
+  const popup = UI.POPUP_TEMPLATE.content.cloneNode('deep');
+  popup.querySelector('.popup_title').innerText = obj.title;
+  popup.querySelector('.popup__main_title').innerText = obj.form_name;
+  popup.querySelector('.popup__input').setAttribute('placeholder', obj.placeholder);
+  popup.querySelector('.chat__btn').innerText = obj.btn_name;
+  popup.querySelector('.popup__exit').addEventListener('click', function () {
+    this.closest('.popup_wrapper').remove();
+  });
+  if (obj.title === settings.title) {
+    popup.querySelector('.popup__main').classList.add('popup__settings');
+  }
+  if (obj.title === authorization.title) {
+    popup.querySelector('.chat__btn').addEventListener('click', authorizationHendler);
+  }
+  if (obj.title === verification.title) {
+    popup.querySelector('.chat__btn').addEventListener('click', function () {
+      setTimeout(function () {
+        document.querySelector('.popup_wrapper').remove();
+      }, 1000);
+      setTimeout(function () {
+        document.querySelector('.any_message').classList.add('visible');
+      }, 4000);
+      setTimeout(function () {
+        const secondMessage = document.querySelector('.any_message').cloneNode('deep');
+        secondMessage.querySelector('.message').innerText = 'Ты справился, молодец!';
+        UI.CHAT_WINDOW.append(secondMessage);
+      }, 10000);
+    });
+  }
+  UI.CHAT.append(popup);
+}
+
+
+function authorizationHendler() {
+  const mailField = this.previousElementSibling;
+  if (!mailField.value) return
+  const mail = {
+    email: this.previousElementSibling.value,
+  }
+  // fetch('https://chat1-341409.oa.r.appspot.com/api/user', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json;charset=utf-8'
+  //   },
+  //   body: JSON.stringify(mail)
+  // })
+  //   .then(response => response.json())
+  //   .then(function (data) {
+  //     console.log(data);
+  //     document.querySelector('.popup_wrapper').remove();
+  //   });
+  setTimeout(function () {
+    document.querySelector('.popup_wrapper').remove();
+    getPopup(verification);
+  }, 2000);
+
 }
--- js/view.js
+++ js/view.js
@@ -1,11 +1,12 @@
 const UI = {
+  CHAT: document.getElementById('chat'),
+  EXIT_BTN: document.getElementById('exit_btn'),
   CHAT_WINDOW: document.getElementById('chat_window'),
   MESSAGE_TEMPLATE: document.getElementById('message_template'),
-  TEXT_FIELD: document.getElementById('text'),
   MESSAGE_FORM: document.getElementById('send_message_form'),
+  MESSAGE_INPUT: document.getElementById('message_text'),
+  POPUP_TEMPLATE: document.getElementById('popup_template'),
   SETTINGS_BTN: document.getElementById('settings_btn'),
-  SETTINGS_MENU: document.getElementById('settings_menu'),
-  SETTINGS_EXIT: document.getElementById('exit'),
 }
 
 export { UI };
